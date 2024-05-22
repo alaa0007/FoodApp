c18dd45176c9d81b5eefd3c4bfa13390a0b9ab83
Ala <a.bouassida@labsoft.fr>
2024-05-22 15:43:57 +0200

    add MealItems

--- src/components/Meals/MealItem.jsx
+++ src/components/Meals/MealItem.jsx
@@ -1,23 +1,23 @@
-import React from 'react'
-import classes from './Meals.module.css'
-import { currencyFormatter } from '../../utils/formatting'
-import Button from '../../UI/Button/Button'
-const MealItem = ({ meal }) => {
-  return (
-    <li className={classes['meal-item']}>
-        <article>
-            <img src={`http://localhost:3000/${meal.image}`} alt={meal.name} />
-            <div>
-                <h3>{meal.name}</h3>
-                <p className={classes['meal-item-description']}>{meal.description}</p>
-                <p className={classes['meal-item-price']}>{currencyFormatter.format(meal.price)}</p>
-            </div>
-            <p className={classes['meal-item-actions']}>
-                <Button>Add to cart</Button>
-            </p>
-        </article>
-    </li>
-  )
-}
-
-export default MealItem
+import React from 'react' 
+import classes from './Meals.module.css' 
+import { currencyFormatter } from '../../utils/formatting' 
+import Button from '../../UI/Button/Button'  
+const MealItem = ({ meal }) => { 
+  return ( 
+    <li className={classes['meal-item']}> 
+        <article> 
+            <img src={`http://localhost:3000/${meal.image}`} alt={meal.name} />  
+            <div> 
+                <h3>{meal.name}</h3> 
+                <p className={classes['meal-item-description']}>{meal.description}</p> 
+                <p className={classes['meal-item-price']}>{currencyFormatter.format(meal.price)}</p> 
+            </div> 
+            <p className={classes['meal-item-actions']}>  
+                <Button>Add to cart</Button>  
+            </p>  
+        </article>  
+    </li> 
+  ) 
+} 
+ 
+export default MealItem;  
